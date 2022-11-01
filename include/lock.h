7845cdbbfdc661d02c94ed480b550899857bf96c
Jacob Little <jlittle4@ncsu.edu>
2022-10-31 22:02:57 -0400

    Spinlock Working
    Signed-off-by: Jacob Little <jlittle4@ncsu.edu>

--- include/lock.h
+++ include/lock.h
@@ -1,1 +1,25 @@
+/* lock.h */
+
+#define NSPINLOCKS 20
+#define NLOCKS 20
+#define NALOCKS 20
+
+//typedef uint32  sl_lock_t;
+
+typedef struct sl_lock_t {
+    pid32   owner;
+    uint32  flag;
+} sl_lock_t;
+
+typedef	uint32	lock_t;
+typedef uint32  al_lock_t;
+
+
+
+/* function prototypes */
 extern uint32 test_and_set(uint32 *, uint32);
+
+extern syscall sl_initlock(sl_lock_t *l);
+extern syscall sl_lock(sl_lock_t *l);
+extern syscall sl_unlock(sl_lock_t *l);
+
--- system/spinlock.c
+++ system/spinlock.c
@@ -1,2 +1,26 @@
 #include "xinu.h"
 
+// sl_lock_t sl_locks[NSPINLOCKS];
+
+syscall sl_initlock(sl_lock_t *l) {
+    static uint32 lock_num = 0;
+    if (lock_num == NSPINLOCKS) return SYSERR;
+    l->flag = 0;
+    l->owner = 0;
+    lock_num++;
+    return OK;
+}
+
+syscall sl_lock(sl_lock_t *l) {
+    while (test_and_set(&l->flag, 1));
+    l->owner = currpid;
+    return OK;
+}
+
+syscall sl_unlock(sl_lock_t *l) {
+    if (l->owner != currpid) {
+        return SYSERR;
+    }
+    l->flag = 0;
+    return OK;
+}
