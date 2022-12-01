5540aea7bd64f13729acd9fd1f65d2570faf50ab
Jacob Little <jlittle4@ncsu.edu>
2022-11-30 23:36:49 -0500

    Paging Stats in System Directory
    Signed-off-by: Jacob Little <jlittle4@ncsu.edu>

--- system/paging_stats.c
+++ system/paging_stats.c
@@ -5,20 +5,30 @@
 uint32 free_ffs_pages() {
     uint32 free_pages = 0;
     int i;
+
+    set_PDBR(proctab[NULLPROC].page_dir);   // change to system virtual address space
+
     for (i=0; i < MAX_FFS_SIZE; i++) { //step through frame list
         if (frame_list[i].valid) continue; //if frame is valid, it is not free
 		else ++free_pages; //invalid frames are counted
     }
+    
+    set_PDBR(proctab[currpid].page_dir);    // return to current process virtual address space
 	return free_pages;
 }
 
 uint32 free_swap_pages() {
 	uint32 free_pages = 0;
     int i;
+
+    set_PDBR(proctab[NULLPROC].page_dir);   // change to system virtual address space
+
     for (i=0; i < MAX_SWAP_SIZE; i++) { //step through frame list
         if (swap_list[i].valid) continue; //if frame is valid, it is not free
 		else ++free_pages; //invalid frames are counted
     }
+
+    set_PDBR(proctab[currpid].page_dir);    // return to current process virtual address space
 	return free_pages;
 }
 
@@ -28,6 +38,8 @@ uint32 allocated_virtual_pages(pid32 pid) {
     pd_t PDE;
     pt_t PTE;
     uint32 count = 0;
+
+    set_PDBR(proctab[NULLPROC].page_dir);   // change to system virtual address space
 
     for (i = 0; i < 1024; i++) {
         PDE = *(pd_t *)((PD_addr.fm_num << 12) + (i << 2));
@@ -41,6 +53,7 @@ uint32 allocated_virtual_pages(pid32 pid) {
         }
     }
 
+    set_PDBR(proctab[currpid].page_dir);    // return to current process virtual address space
 	return count;
 }
 
@@ -50,6 +63,8 @@ uint32 used_ffs_frames(pid32 pid) {
     pd_t PDE;
     pt_t PTE;
     uint32 count = 0;
+
+    set_PDBR(proctab[NULLPROC].page_dir);   // change to system virtual address space
 
     for (i = (XINU_PAGES >> 10); i < 1024; i++) {
         PDE = *(pd_t *)((PD_addr.fm_num << 12) + (i << 2));
@@ -63,5 +78,6 @@ uint32 used_ffs_frames(pid32 pid) {
         }
     }
 
+    set_PDBR(proctab[currpid].page_dir);    // return to current process virtual address space
 	return count;
 }
